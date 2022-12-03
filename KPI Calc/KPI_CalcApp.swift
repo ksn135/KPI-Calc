469aea38fd5e4be1880542e7ffa83b1d886f051e
ksn135 <serg@kalachev.ru>
2022-12-03 10:31:06 +0300

    Initial Commit

---
+++ KPI Calc/KPI_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPI_CalcApp.swift
+//  KPI Calc
+//
+//  Created by Serg Kalachev on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct KPI_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
